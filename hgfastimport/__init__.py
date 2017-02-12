a8f7ab201ad8168bb1e870fefba7112307936028
Daniel Johnson <daniel@daniel-johnson.org>
2017-02-11 20:21:40 -0500

    Switch to modern @command decorator API
    Adds compatiblility with hg 4.2
    Clean up formatting and add description

--- hgfastimport/__init__.py
+++ hgfastimport/__init__.py
@@ -1,9 +1,35 @@
-from mercurial import encoding, util
+''' import Git fast-import streams '''
+from __future__ import absolute_import
+
+from mercurial import (
+    encoding,
+    util,
+    cmdutil,
+)
+
 from mercurial.i18n import _
-from hgext.convert import convcmd, hg
 
-from hgimport import fastimport_source
+from hgext.convert import (
+    convcmd,
+    hg,
+)
 
+from .hgimport import fastimport_source
+
+
+# XXX sort options copied straight from hgext/convert/__init__.py
+cmdtable = {}
+command = cmdutil.command(cmdtable)
+
+testedwith = '4.1'
+
+
+@command("fastimport",
+         [('', 'branchsort', None, _('try to sort changesets by branches')),
+          ('', 'datesort', None, _('try to sort changesets by date')),
+          ('', 'sourcesort', None, _('preserve source changesets order'))],
+         _('hg fastimport SOURCE ...'),
+         norepo=False)
 def fastimport(ui, repo, *sources, **opts):
     """Convert a git fastimport dump into Mercurial changesets.
 
@@ -39,19 +65,8 @@ def fastimport(ui, repo, *sources, **opts):
     if len(sortmode) > 1:
         raise util.Abort(_('more than one sort mode specified'))
     sortmode = sortmode and sortmode[0] or defaultsort
-    
+
     # not implemented: filemap, revmapfile
     revmapfile = destc.revmapfile()
     c = convcmd.converter(ui, srcc, destc, revmapfile, opts)
     c.convert(sortmode)
-
-# XXX sort options copied straight from hgext/convert/__init__.py
-cmdtable = {
-    "fastimport":
-        (fastimport,
-         [('', 'branchsort', None, _('try to sort changesets by branches')),
-          ('', 'datesort', None, _('try to sort changesets by date')),
-          ('', 'sourcesort', None, _('preserve source changesets order')),
-         ],
-         'hg fastimport SOURCE ...')
-}
